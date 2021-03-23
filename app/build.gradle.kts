339748c58eb39bd5acee28543a565a1a48fad2a2
Benjamin Monjoie <bmo@appkers.com>
2021-03-23 09:12:45 +0100

    Update to compose beta02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,11 +49,11 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.3.0-beta01")
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha03")
-    implementation("androidx.activity:activity-compose:1.3.0-alpha03")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha05")
+    implementation("androidx.activity:activity-compose:1.3.0-alpha04")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion by extra("1.4.30")
-    val composeVersion by extra("1.0.0-beta01")
+    val kotlinVersion by extra("1.4.31")
+    val composeVersion by extra("1.0.0-beta02")
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha07")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha10")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
         // NOTE: Do not place your application dependencies here; they belong
